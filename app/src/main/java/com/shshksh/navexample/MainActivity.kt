2efc51fa987dca1e091b8c6353c1446bba96893b
Seonghwan Kim <ksh970516@pukyong.ac.kr>
2021-01-15 17:10:49 +0900

    apply bottom navigation extension and separate navigation drawer graph

--- app/src/main/java/com/shshksh/navexample/MainActivity.kt
+++ app/src/main/java/com/shshksh/navexample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.shshksh.navexample.databinding.ActivityMainBinding
 
@@ -16,40 +17,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val host = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
-        val navController = host.navController
-//        val appBarConfiguration = AppBarConfiguration(setOf(R.id.home_dest, R.id.noti_dest), binding.layoutDrawerMain)
-//
-//        binding.toolbarTop.setupWithNavController(navController, appBarConfiguration)
-
-        binding.navDrawer.setupWithNavController(navController)
-
-        val homeStartDestination = NavHostFragment.create(R.navigation.nav_home)
-        val notificationStartDestination = NavHostFragment.create(R.navigation.nav_noti)
-
-//        binding.navBottom.setupWithNavController(navController)
-        binding.navBottom.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.nav_home -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.nav_host_fragment, homeStartDestination)
-                        .setPrimaryNavigationFragment(homeStartDestination)
-                        .commit()
-                    true
-                }
-                R.id.nav_noti -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.nav_host_fragment, notificationStartDestination)
-                        .setPrimaryNavigationFragment(notificationStartDestination)
-                        .commit()
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }
         binding.navBottom.menu[1].isEnabled = false
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.notificationFragment), binding.layoutDrawerMain)
+        val navGraphIds = listOf(R.navigation.nav_home, R.navigation.nav_noti)
+        val controller = binding.navBottom.setupWithNavController(
+                navGraphIds = navGraphIds,
+                fragmentManager = supportFragmentManager,
+                containerId = R.id.nav_host_fragment,
+                intent = intent
+        )
+
+        controller.observe(this, { navController ->
+            binding.toolbarTop.setupWithNavController(navController, appBarConfiguration)
+            binding.navDrawer.setupWithNavController(navController)
+        })
     }
 }
